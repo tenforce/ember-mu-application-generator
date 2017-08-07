2a988bcb8142a14e4be59889073eb518a00ac44b
Nóra Szepes <nora.szepes@tenforce.com>
2017-08-07 17:06:47 +0200

    removed ember-data-table

--- blueprints/resgen/files/app/routes/__plural_name__/index.js
+++ blueprints/resgen/files/app/routes/__plural_name__/index.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
-import DataTableRouteMixin from 'ember-data-table/mixins/route';
 
-export default Ember.Route.extend(DataTableRouteMixin, {
+export default Ember.Route.extend({
   modelName: '<%= entityName %>',
   queryParams: {
 <%= relationships.map( function(relationship) {
@@ -17,16 +16,16 @@ export default Ember.Route.extend(DataTableRouteMixin, {
 			           "    mergedParams['filter[" + name + "][id]'] = params['" + name + "'];\n" +
 		             "}";
 		return result;
-}).join("\n") %>   
+}).join("\n") %>
     return mergedParams;
   },
  // see https://guides.emberjs.com/v2.10.0/routing/query-params/#toc_sticky-query-param-values
- // sticky query params for relationships are undesirable at the moment	
+ // sticky query params for relationships are undesirable at the moment
  resetController(controller, isExiting, transition) {
     if (isExiting) {
       // isExiting would be false if only the route's model was changing
      controller.set('page', 0);
 <%= relationships.map( function(relationship) {	return "    controller.set('" + relationship.name +"', '');"}).join("\n") %>
     }
-  }		
+  }
 });
