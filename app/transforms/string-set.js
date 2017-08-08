dcf2e2cc72c32e96076fff870eca4f3862f404a2
vincent.goossens <vincent.goossens@tenforce.com>
2017-08-08 15:38:12 +0200

    Decaffeinated transformers

---
+++ app/transforms/string-set.js
@@ -0,0 +1,19 @@
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+import DS from 'ember-data';
+import Ember from 'ember';
+import Transform from 'ember-data/transform';
+
+const StringSet = DS.Transform.extend({
+  deserialize(serialized) {
+    Ember.assert(`expected array got ${Ember.typeOf(serialized)}`, (!serialized) || (Ember.typeOf(serialized) === "array"));
+    return serialized || [];
+  },
+  serialize(deserialized) {
+    Ember.assert(`expected array got ${Ember.typeOf(deserialized)}`, (!deserialized) || (Ember.typeOf(deserialized) === "array"));
+    return deserialized || [];
+  }});
+export default StringSet;
