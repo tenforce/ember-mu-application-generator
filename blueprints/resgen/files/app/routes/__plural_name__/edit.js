e63155b9cf366ef0a9782655314e3677903dd673
Nóra Szepes <nora.szepes@tenforce.com>
2017-08-08 14:23:46 +0200

    Added design to edit page. moved deletion of model from show to edit

--- blueprints/resgen/files/app/routes/__plural_name__/edit.js
+++ blueprints/resgen/files/app/routes/__plural_name__/edit.js
@@ -1,20 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return this.modelFor('<%= entityName %>');
+  store: Ember.inject.service(),
+  model(params) {
+    return this.store.find('<%= entityName %>', params.id );
   },
   actions: {
     cancel(model) {
       model.rollbackAttributes();
-      this.transitionTo("<%= entityName %>", model);
+      this.transitionTo("<%= entitiesName %>.show", model);
     },
     save(model) {
       var self = this;
       model.save().then( function() {
-        self.transitionTo("<%= entityName %>", model);
+        self.transitionTo("<%= entitiesName %>.show", model);
       }).catch( function() {
         alert("Could not save <%= entityName %>");
+      });
+    },
+    delete(model) {
+      var self = this;
+      model.deleteRecord();
+      return model.save().then( function(model) {
+        self.transitionTo( "<%= entitiesName %>");
+      }).catch( function() {
+        model.rollbackAttributes();
+        alert("Deletion of <%= entityName %> failed");
       });
     }
   }
