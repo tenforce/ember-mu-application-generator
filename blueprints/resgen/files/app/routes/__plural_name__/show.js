c0a9d903bbd30592c54761a285aa7c09c01f302e
Nóra Szepes <nora.szepes@tenforce.com>
2017-08-08 11:16:42 +0200

    Added delete and return to list action buttons to show page

--- blueprints/resgen/files/app/routes/__plural_name__/show.js
+++ blueprints/resgen/files/app/routes/__plural_name__/show.js
@@ -4,5 +4,20 @@ export default Ember.Route.extend({
   store: Ember.inject.service(),
   model(params) {
     return this.store.find('<%= entityName %>', params.id );
+  },
+  actions: {
+    delete(model) {
+      var self = this;
+      model.deleteRecord();
+      return model.save().then( function(model) {
+        self.transitionTo( "<%= entitiesName %>");
+      }).catch( function() {
+        model.rollbackAttributes();
+        alert("Deletion of <%= entityName %> failed");
+      });
+    },
+    backToList () {
+      this.transitionTo("<%= entitiesName %>");
+    }
   }
 });
