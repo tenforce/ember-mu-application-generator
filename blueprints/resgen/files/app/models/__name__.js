324e87a919ccd8f7a47c7bbd62585336c73dda32
vincent.goossens <vincent.goossens@tenforce.com>
2017-08-10 17:15:19 +0200

    Added support for belongs-to relationships

--- blueprints/resgen/files/app/models/__name__.js
+++ blueprints/resgen/files/app/models/__name__.js
@@ -1,5 +1,10 @@
 <%= importStatements %>
 
 export default Model.extend({
+  // A string representation of this model, based on its attributes.
+  // This is what mu-cl-resources uses to search on, and how the model will be presented while editing relationships.
+  // TODO what if there's no attributes?
+  stringRep: Ember.computed.collect.apply(this,[<%="'" + attributes.map( function(attribute) {return attribute.name}).join("', '") + "'"%>]),
+
   <%= attrs %>
 });
