d318daf91cc0986b3b7d62ab7ab59cdb602b730d
Nóra Szepes <nora.szepes@tenforce.com>
2017-08-11 14:33:14 +0200

    New.hbs: updated with editable fields for boolean, lang-string-set and string-set.
    Also fixed a bug if a string-set is null, create a [].

--- app/components/edit-attribute-string-set.js
+++ app/components/edit-attribute-string-set.js
@@ -15,7 +15,12 @@ export default Ember.Component.extend({
       strings.removeAt(index);
     },
     createLabel() {
+
+      if ((this.get("model." + this.get('attribute'))) == null) {
+        this.set("model." + this.get('attribute'), []);
+      }
       var strings = this.get("model." + this.get('attribute'));
+
       if (this.get("lang")) {
         strings.pushObject({
           content: "",
