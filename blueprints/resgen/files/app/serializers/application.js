d6529a9012bdb9cded8f5d225349bdd21fc86399
Nóra Szepes <nora.szepes@tenforce.com>
2017-08-16 15:13:41 +0200

    removed extra typo in serializer

--- blueprints/resgen/files/app/serializers/application.js
+++ blueprints/resgen/files/app/serializers/application.js
@@ -1,4 +1,4 @@
-import DS from 'ember-data';numberOfPages
+import DS from 'ember-data';
 import PaginationSerializer from '../mixins/pagination-serializer';
 
 export default DS.JSONAPISerializer.extend(PaginationSerializer, {
