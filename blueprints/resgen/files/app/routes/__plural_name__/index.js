79a52af184872306ab6c5aa0ef92a72055b2a235
Nóra Szepes <nora.szepes@tenforce.com>
2017-08-16 11:15:03 +0200

    Added page and size query params to index route

--- blueprints/resgen/files/app/routes/__plural_name__/index.js
+++ blueprints/resgen/files/app/routes/__plural_name__/index.js
@@ -1,8 +1,34 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  defaultSize: 5,
+  defaultPage: 0,
+  queryParams: {
+    page: {
+      refreshModel: true
+    },
+    size: {
+      refreshModel: true
+    },
+  },
   store: Ember.inject.service(),
-  model() {
-    return this.get('store').findAll('<%= entityName %>');
+  model(params) {
+    let page = params.page;
+    let size = params.size;
+    if(size == null) {
+      size = this.get('defaultSize');
+    }
+    if(page == null) {
+      page = this.get('defaultPage');
+    }
+
+    const options = {
+      page: {
+        number: page,
+        size: size
+      }
+    };
+
+    return this.get('store').query('<%= entityName %>', options);
   }
 });
