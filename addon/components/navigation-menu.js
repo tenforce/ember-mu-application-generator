aa53a068f29d2cf865e767207888a2a1eefaea28
Nóra Szepes <nora.szepes@tenforce.com>
2017-08-22 17:14:19 +0200

    navigation-menu design and selected item

--- addon/components/navigation-menu.js
+++ addon/components/navigation-menu.js
@@ -14,6 +14,7 @@ import layout from '../templates/components/navigation-menu';
 export default Ember.Component.extend({
   layout: layout,
   tagName: 'div',
+  currentRoute: '',
 
   menus: Ember.computed(function() {
     let router = Ember.getOwner(this).lookup('router:main');
@@ -31,7 +32,7 @@ export default Ember.Component.extend({
 
   actions: {
     goToPage: function(item) {
-      this.transition(item);
+      this.transitionTo(item);
     }
   }
 });
