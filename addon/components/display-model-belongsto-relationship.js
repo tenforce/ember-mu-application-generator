c5cadfe8f6fb90abf3e3f8fe38a84064ec497d6b
Nóra Szepes <nora.szepes@tenforce.com>
2017-08-22 11:51:18 +0200

    Added files to make build work. Cleaned some js hint errors.

--- addon/components/display-model-belongsto-relationship.js
+++ addon/components/display-model-belongsto-relationship.js
@@ -3,7 +3,6 @@ import layout from '../templates/components/display-model-belongsto-relationship
 
 export default Ember.Component.extend({
   layout: layout,
-  layout: layout,
   tagName: 'div',
   classNameBindings: ['classes'],
   classes: ""
--- addon/components/display-model-hasmany-relationship.js
+++ addon/components/display-model-hasmany-relationship.js
@@ -4,7 +4,6 @@ import layout from '../templates/components/display-model-hasmany-relationship';
 export default Ember.Component.extend({
   layout: layout,
   routing: Ember.inject.service('-routing'),
-  layout: layout,
   tagName: 'div',
   classNameBindings: ['classes'],
   classes: ""
