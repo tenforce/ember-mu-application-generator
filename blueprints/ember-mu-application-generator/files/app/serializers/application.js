8a599662b5f9b54b182bf3a57f54be4083f935ef
Nóra Szepes <nora.szepes@tenforce.com>
2017-08-22 13:34:19 +0200

    Added ember-mu-application-generator blueprint
    This way when we install the addon, it'll install the necessary files, like locals, transforms, serializers, utils, etc.

--- blueprints/ember-mu-application-generator/files/app/serializers/application.js
+++ blueprints/ember-mu-application-generator/files/app/serializers/application.js
@@ -1,5 +1,5 @@
 import DS from 'ember-data';
-import PaginationSerializer from '../mixins/pagination-serializer';
+import PaginationSerializer from 'ember-mu-application-generator/mixins/pagination-serializer';
 
 export default DS.JSONAPISerializer.extend(PaginationSerializer, {
 });
