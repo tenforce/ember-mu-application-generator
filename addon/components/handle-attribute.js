733098acf76b0a80afac44585161657275f9a8f4
Nóra Szepes <nora.szepes@tenforce.com>
2017-08-22 11:03:15 +0200

    lang-string-set -> language-string-set

--- addon/components/handle-attribute.js
+++ addon/components/handle-attribute.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import layout from '../templates/components/handle-attribute';
 
 /*
-  This component displays and edits attributes (except string-set, lang-string-set).
+  This component displays and edits attributes (except string-set, language-string-set).
   The tagname is configurable, the default is a div, because otherwise we cannot use classNameBindings.
   The classes are configurable, the default is "".
 */
