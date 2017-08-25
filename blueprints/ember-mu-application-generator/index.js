44c92139673104f96e94c62a702d5afbcadeda7f
Nóra Szepes <nora.szepes@tenforce.com>
2017-08-25 13:31:47 +0200

    installing addons as addons and not as packages

--- blueprints/ember-mu-application-generator/index.js
+++ blueprints/ember-mu-application-generator/index.js
@@ -7,7 +7,7 @@ module.exports = {
 
   // to install important packages
   afterInstall: function(options) {
-    this.addPackageToProject('ember-power-select', '^1.9.3');
-    return this.addPackageToProject('ember-changeset', '^1.3.0');
+    this.addAddonToProject('ember-power-select', '^1.9.3');
+    return this.addAddonToProject('ember-changeset', '^1.3.0');
   }
 };
