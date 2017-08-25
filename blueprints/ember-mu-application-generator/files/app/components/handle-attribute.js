aea2f2ce48d9d0bf0a1154343c7b5abf9dc47fb5
Nóra Szepes <nora.szepes@tenforce.com>
2017-08-25 09:30:05 +0200

    Removed image regex
    it supposed to be yds only

--- blueprints/ember-mu-application-generator/files/app/components/handle-attribute.js
+++ blueprints/ember-mu-application-generator/files/app/components/handle-attribute.js
@@ -10,14 +10,5 @@ export default Ember.Component.extend({
   layout: layout,
   tagName: "div",
   classNameBindings: ['classes'],
-  classes: "",
-
-  isImage: Ember.computed('model', 'attribute', function() {
-    let attribute = this.get('attribute');
-    let regExp = /(\.png)|(\.svg)|(\.jpg)|(\.gif)|(\.tif)|(\.bmp)$/;
-    if (this.get('model') != null) {
-      attribute = this.get("model." + attribute);
-    }
-    return regExp.test(attribute);
-  })
+  classes: ""
 });
