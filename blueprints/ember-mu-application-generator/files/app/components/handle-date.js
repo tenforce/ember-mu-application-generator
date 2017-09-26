e567349f20a01e8c96b3b94f6fabf1577f1051d4
Nóra Szepes <nora.szepes@tenforce.com>
2017-09-26 15:23:48 +0200

    The date handler now shows the date inline. Timepicker is configurable

--- blueprints/ember-mu-application-generator/files/app/components/handle-date.js
+++ blueprints/ember-mu-application-generator/files/app/components/handle-date.js
@@ -5,10 +5,13 @@ import layout from '../templates/components/handle-attribute';
   This component displays and edits dates.
   The tagname is configurable, the default is a div, because otherwise we cannot use classNameBindings.
   The classes are configurable, the default is "".
+
+  timePicker - show timepicker or not, the default is false
 */
 export default Ember.Component.extend({
   layout: layout,
   tagName: "div",
   classNameBindings: ['classes'],
-  classes: ""
+  classes: "",
+  timePicker: false
 });
