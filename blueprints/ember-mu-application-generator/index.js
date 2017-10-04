3f406b3197a87a4b0f31dc4375baa1e42289037c
Nóra Szepes <nora.szepes@tenforce.com>
2017-10-04 09:51:53 +0200

    installing jquery-datetimepicker on running the default blueprint

--- blueprints/ember-mu-application-generator/index.js
+++ blueprints/ember-mu-application-generator/index.js
@@ -7,6 +7,7 @@ module.exports = {
 
   // to install important packages
   afterInstall: function(options) {
+    this.addPackageToProject('jquery-datetimepicker', '^2.5.4');
     this.addAddonToProject('ember-power-select', '^1.9.3');
     this.addAddonToProject('ember-datetimepicker', '^2.1.0');
     return this.addAddonToProject('ember-changeset', '^1.3.0');
