ed66e3725b9ac24315d050e3362e5d727d22f5d9
Erika Pauwels <erika.pauwels@gmail.com>
2018-03-09 10:35:19 +0100

    Move installation of ember-cli-sass to afterInstall hook

--- blueprints/mu-application-design/index.js
+++ blueprints/mu-application-design/index.js
@@ -12,10 +12,9 @@ module.exports = {
   //   };
   // }
   beforeInstall: function() {
-    return this.addAddonToProject("ember-cli-sass");
   },
 
   afterInstall: function(options) {
-
+    return this.addAddonToProject("ember-cli-sass");
   }
 };
